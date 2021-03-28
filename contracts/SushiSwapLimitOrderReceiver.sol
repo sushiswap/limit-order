10cfd7a2e324dc868c5c67672b01c7e21c39fe87
Keno <kenobudde@gmail.com>
2021-03-28 17:32:11 +0200

    Change to push pattern PairCodeHash addition for multichain support

--- contracts/SushiSwapLimitOrderReceiver.sol
+++ contracts/SushiSwapLimitOrderReceiver.sol
@@ -4,44 +4,51 @@ pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
 import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
-import "@sushiswap/core/contracts/uniswapv2/libraries/UniswapV2Library.sol";
+import "@boringcrypto/boring-solidity/contracts/libraries/BoringMath.sol";
+import "./libraries/UniswapV2Library.sol";
 import "@sushiswap/core/contracts/uniswapv2/libraries/TransferHelper.sol";
 import "@sushiswap/bentobox-sdk/contracts/IBentoBoxV1.sol";
 import "./interfaces/ILimitOrderReceiver.sol";
+import "hardhat/console.sol";
+
 
 contract SushiSwapLimitOrderReceiver is ILimitOrderReceiver {
     using BoringERC20 for IERC20;
+    using BoringMath for uint256;
 
     address private immutable factory;
 
     IBentoBoxV1 private immutable bentoBox;
 
-    constructor (address _factory, IBentoBoxV1 _bentoBox) public {
+    bytes32 private immutable pairCodeHash;
+
+    constructor (address _factory, IBentoBoxV1 _bentoBox, bytes32 _pairCodeHash) public {
         factory = _factory;
         bentoBox = _bentoBox;
+        pairCodeHash = _pairCodeHash;
     }
 
     function onLimitOrder (IERC20 tokenIn, IERC20 tokenOut, uint256 amountIn, uint256 amountMinOut, bytes calldata data) override external {
         bentoBox.withdraw(tokenIn, address(this), address(this), amountIn, 0);
         (address[] memory path, uint256 amountOutMinExternal, address to) = abi.decode(data, (address[], uint256, address));
-        _swapExactTokensForTokens(address(this), amountIn, amountOutMinExternal, path, address(this));
-        bentoBox.deposit(tokenOut, address(this), msg.sender, amountMinOut, 0);
-        bentoBox.deposit(tokenOut, address(this), to, tokenOut.balanceOf(address(this)), 0);
+        uint256 amountOut = _swapExactTokensForTokens(amountIn, amountOutMinExternal, path, address(this));
+        tokenOut.safeTransfer(address(bentoBox), amountOut);
+        bentoBox.deposit(tokenOut, address(bentoBox), msg.sender, amountMinOut, 0);
+        bentoBox.deposit(tokenOut, address(bentoBox), to, amountOut.sub(amountMinOut), 0);
     }
 
     // Swaps an exact amount of tokens for another token through the path passed as an argument
     // Returns the amount of the final token
     function _swapExactTokensForTokens(
-        address from,
         uint256 amountIn,
         uint256 amountOutMin,
         address[] memory path,
         address to
     ) internal returns (uint256 amountOut) {
-        uint256[] memory amounts = UniswapV2Library.getAmountsOut(factory, amountIn, path);
+        uint256[] memory amounts = UniswapV2Library.getAmountsOut(factory, amountIn, path, pairCodeHash);
         amountOut = amounts[amounts.length - 1];
         require(amountOut >= amountOutMin, "insufficient-amount-out");
-        TransferHelper.safeTransferFrom(path[0], from, UniswapV2Library.pairFor(factory, path[0], path[1]), amountIn);
+        IERC20(path[0]).safeTransfer(UniswapV2Library.pairFor(factory, path[0], path[1], pairCodeHash), amountIn);
         _swap(amounts, path, to);
     }
 
@@ -58,8 +65,8 @@ contract SushiSwapLimitOrderReceiver is ILimitOrderReceiver {
             (uint256 amount0Out, uint256 amount1Out) = input == token0
                 ? (uint256(0), amountOut)
                 : (amountOut, uint256(0));
-            address to = i < path.length - 2 ? UniswapV2Library.pairFor(factory, output, path[i + 2]) : _to;
-            IUniswapV2Pair(UniswapV2Library.pairFor(factory, input, output)).swap(
+            address to = i < path.length - 2 ? UniswapV2Library.pairFor(factory, output, path[i + 2], pairCodeHash) : _to;
+            IUniswapV2Pair(UniswapV2Library.pairFor(factory, input, output, pairCodeHash)).swap(
                 amount0Out,
                 amount1Out,
                 to,
